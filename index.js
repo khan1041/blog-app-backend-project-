2586a3d5a52da5a4eac91928b858bd759fdf5fb3
Md.Shimul Khan <shimulssk.121@gmail.com>
2025-01-11 12:56:29 +0600

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,8 @@ app.use(express.json())
 app.use(
   cors({
   origin:true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
+credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
 
   })
 );
